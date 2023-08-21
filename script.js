a46c8cec074951c91726e2abc910a6218e17f2ac
gargi <131345129+GargiJad@users.noreply.github.com>
2023-08-21 16:31:22 +0530

    dss

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const box1 = document.querySelector("#button1");
-    const box2 = document.querySelector("#button2");
-    const box3 = document.querySelector("#button3");
-    const box4 = document.querySelector("#button4");
+    const buttons = document.querySelectorAll(".clickable");
   
-    box1.addEventListener("click", function () {
-      // Open the LinkedIn link in a new tab
-      window.open("https://www.linkedin.com/", "_blank");
-    });
+    buttons.forEach(function (button) {
+      button.addEventListener("click", function () {
+        const buttonId = button.id;
   
-    box2.addEventListener("click", function () {
-      // Open the Instagram link in a new tab
-      window.open("https://www.instagram.com/", "_blank");
-    });
-  
-    box3.addEventListener("click", function () {
-      // Open the Twitter link in a new tab
-      window.open("https://www.twitter.com/", "_blank");
-    });
-  
-    box4.addEventListener("click", function () {
-      // Open the personal website link in a new tab
-      window.open("https://www.example.com/", "_blank");
+        switch (buttonId) {
+          case "button1":
+            // Open the LinkedIn link in a new tab
+            window.open("https://www.linkedin.com/", "_blank");
+            break;
+          case "button2":
+            // Open the Instagram link in a new tab
+            window.open("https://www.instagram.com/", "_blank");
+            break;
+          case "button3":
+            // Open the Twitter link in a new tab
+            window.open("https://www.twitter.com/", "_blank");
+            break;
+          case "button4":
+            // Open the personal website link in a new tab
+            window.open("https://www.example.com/", "_blank");
+            break;
+        }
+      });
     });
   });
   
